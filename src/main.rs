3695a68a4a5b6b9ef677f3791a8e12cda12454ae
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2022-10-04 23:01:41 +0900

    compiler

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,24 @@ mod parser;
 mod tokenizer;
 mod transformer;
 
+fn compiler(input: &str) -> String {
+    let tokens = tokenizer::tokenizer(input).unwrap();
+    let ast = parser::parser(tokens).unwrap();
+    let new_ast = transformer::transformer(ast).unwrap();
+    code_generator::code_generator(new_ast)
+}
+
+#[test]
+fn compiler_test() {
+    let input = "(add 2 (subtract 4 2))";
+    let excepted = "add(2, subtract(4, 2));".to_string();
+
+    assert_eq!(compiler(input), excepted);
+}
+
 fn main() {
     let mut input = String::new();
     std::io::stdin().read_line(&mut input).unwrap();
 
-    let tokens = tokenizer::tokenizer(input.trim()).unwrap();
-    let ast = parser::parser(tokens).unwrap();
-    let new_ast = transformer::transformer(ast).unwrap();
-    let output = code_generator::code_generator(new_ast);
-
-    println!("{}", output);
+    println!("{}", compiler(&input));
 }
